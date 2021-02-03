1c5082e5eddcafbbdf85c35df81f16627ea93c3c
Tien <tienbusinessinquiry@gmail.com>
2021-02-03 11:56:37 -0800

    reorganization

---
+++ PythonPractice/operator.py
@@ -0,0 +1,5 @@
+x = 10 + 3 * 2 ** 2
+#what is the order of precedence?
+#Answer is 22
+# Parentheses, Exponentiation, multiplication or division, addition or subtraction
+print(x)
--- operator.py
+++
@@ -1,5 +0,0 @@
-x = 10 + 3 * 2 ** 2
-#what is the order of precedence?
-#Answer is 22
-
-print(x)
