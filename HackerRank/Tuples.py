fcfab361d248d70a77ef4c29cc9e925788064123
Tien <tienbusinessinquiry@gmail.com>
2021-02-10 12:27:55 -0800

    practice with tuples

---
+++ HackerRank/Tuples.py
@@ -0,0 +1,11 @@
+#Given an integer, n, and n space-separated integers as input, create a tuple,t, of those n integers. Then compute and print the
+#result of hash(t)
+
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    newTup = ()
+    for item in integer_list:
+        newTup += (item,)
+    
+    print(hash(newTup))
