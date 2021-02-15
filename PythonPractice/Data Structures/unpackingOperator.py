d23bb905b4bb7d58da20d2b8b5cd0f81b8db3553
Tien <tienbusinessinquiry@gmail.com>
2021-02-15 11:01:26 -0800

    fun ways to unpack lists, sets, or dictionaries

---
+++ PythonPractice/Data Structures/unpackingOperator.py
@@ -0,0 +1,15 @@
+numbers = [1,2,3]
+print(numbers)
+#what if we want 1 2 3, we can achieve this with unpacking
+print(*numbers) #in java it's like ...
+
+#useful for lists
+values1 = list(range(5))
+values2 = [*range(5), *"Hello"] #we can unpack any iterable
+print(values1)
+print(values2)
+
+first = [1,2]
+second = [3]
+values = [*first, "a", *second, *"Hello"]
+print(values)
