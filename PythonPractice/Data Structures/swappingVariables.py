b2358dfda36757a46b7df270be43207b156702f9
Tien <tienbusinessinquiry@gmail.com>
2021-02-15 10:16:45 -0800

    python can swap variables simply without a third variable using tuples

---
+++ PythonPractice/Data Structures/swappingVariables.py
@@ -0,0 +1,15 @@
+x = 10
+y = 11
+
+# z = x #old value of x stored in z
+# x = y 
+# y = z
+
+# print('x', x)
+# print('y', y)
+
+#in python we can swap without the need for a third variable
+x, y = y, x
+print('Without third variable: ')
+print('x', x)
+print('y', y)
