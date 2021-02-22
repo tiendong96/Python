6e046ed0da94a7514b4187dc0ea458f05759d1b2
Tien <tienbusinessinquiry@gmail.com>
2021-02-22 12:10:42 -0800

    only use raise exception if you really need to

---
+++ PythonPractice/Exceptions/costOfRaisingExceptions.py
@@ -0,0 +1,28 @@
+from timeit import timeit
+
+code1 = """
+def calculate_xfactor(age):
+    if age <= 0:
+        raise ValueError("age cannot be 0 or less.")
+    return 10 / age
+try:
+    calculate_xfactor(-1)
+except ValueError as error:
+    pass
+
+#doing this is very costly
+"""
+
+code2 = """
+def calculate_xfactor(age):
+    if age <= 0:
+        return None
+    return 10 / age
+
+xfactor = calculate_xfactor(-1)
+if xfactor == None:
+    pass
+"""
+
+print("first code= ", timeit(code1, number= 10000))
+print("second code= ", timeit(code2, number= 10000))
