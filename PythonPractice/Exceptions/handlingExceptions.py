35c8ec3117640cf81e89deb27585dc26d7d0cba6
Tien <tienbusinessinquiry@gmail.com>
2021-02-22 11:51:20 -0800

    add multiple exceptions to one line if message is similar

--- PythonPractice/Exceptions/handlingExceptions.py
+++ PythonPractice/Exceptions/handlingExceptions.py
@@ -1,8 +1,11 @@
 try:
     age = int(input("Age: "))
-except ValueError as ex: 
-    print("You did not enter a valid age.")
-    print(ex)
-    print(type(ex))
+    xfactor = 10/age
+except(ValueError, ZeroDivisionError): #can add many exceptions to one line if the message is going to be the same, or else, you'll have to change each message
+    print("You didn't enter a valid age.")
+# except ZeroDivisionError:
+#     print("You didn't enter a valid age.")
 else:
     print("No exceptions were thrown")
+
+#when python excutes the try block, if any of the code throws an exception it will ignore other exceptions 
