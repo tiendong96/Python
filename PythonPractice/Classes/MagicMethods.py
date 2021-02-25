8de65b3a602d90ec88c888f3129a2a6189a3e1f6
Tien <tienbusinessinquiry@gmail.com>
2021-02-24 23:50:50 -0800

    more info on different types of magic methods online

---
+++ PythonPractice/Classes/MagicMethods.py
@@ -0,0 +1,13 @@
+class Point:
+    def __init__(self, x, y): #called automatically by python interpreter
+        self.x = x
+        self.y = y
+
+    # def __str__(self):
+    #     return f"({self.x}, {self.y})"
+
+    def draw(self):
+        print(f"Point ({self.x}, {self.y})")
+
+point = Point(1, 2) 
+print(str(point))
