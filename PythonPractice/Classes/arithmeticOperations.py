a822bf74e468dcf929dba0af557e4302c8531e7e
Tien <tienbusinessinquiry@gmail.com>
2021-02-25 00:21:12 -0800

    using __add__ to add objects

---
+++ PythonPractice/Classes/arithmeticOperations.py
@@ -0,0 +1,15 @@
+class Point:
+    def __init__(self, x, y): 
+        self.x = x
+        self.y = y
+
+    def __add__ (self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+point = Point(10, 20) 
+other = Point(1, 2)
+print(point + other) #gives an error, instead use a magic method instead to help quickly
+
