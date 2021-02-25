817049517589e9a695efc3d13344d11be409523e
Tien <tienbusinessinquiry@gmail.com>
2021-02-24 23:53:29 -0800

    added helpful link for magic methods

--- PythonPractice/Classes/MagicMethods.py
+++ PythonPractice/Classes/MagicMethods.py
@@ -3,11 +3,13 @@ class Point:
         self.x = x
         self.y = y
 
-    # def __str__(self):
-    #     return f"({self.x}, {self.y})"
+    def __str__(self):
+        return f"({self.x}, {self.y})"
 
     def draw(self):
         print(f"Point ({self.x}, {self.y})")
 
 point = Point(1, 2) 
 print(str(point))
+
+# https://rszalski.github.io/magicmethods/ 
