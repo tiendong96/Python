ad9517597dbdbe056d4505248b71ac0d7ead14d3
Tien <tienbusinessinquiry@gmail.com>
2021-03-03 16:17:01 -0800

    Introduction to using sets

---
+++ Practice/Set/IntroToSets.py
@@ -0,0 +1,14 @@
+# Now, let's use our knowledge of sets and help Mickey.
+# Ms. Gabriel Williams is a botany professor at District College. 
+# One day, she asked her student Mickey to compute the average of all the plants with distinct heights in her greenhouse.
+
+def average(array):
+    unique_array = set(array)
+    return sum(unique_array)/len(unique_array)
+
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
